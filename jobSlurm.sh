3b4eafff21666c410f6c474b41d814a405a6a060
Christophe CHALONS <2277704C@student.gla.ac.uk>
2025-12-05 16:56:21 +0100

    Completes jobSlurm.sh

--- jobSlurm.sh
+++ jobSlurm.sh
@@ -8,3 +8,16 @@
 #SBATCH --time=00:10:00 #Temps d'exécution maximum demnade (HH:MM:SS)
 #SBATCH --output=nom_fichier_sortie.out
 #SBATCH --error=nom_fichier_error.out
+
+#On se place dans le répertoire de soumission
+cd ${SLURM_SUBMIT_DIR}
+
+# echo des commandes lancées
+
+set -x
+
+extra_args="-log_view -dm_mat_type aij -dm_vec_type mpi"
+size=1000
+
+#exécution du code
+time srun ./dmda $extra_args -da_grid_x $size -da_grid_y $size -ksp_type cg -pc_type hypre
